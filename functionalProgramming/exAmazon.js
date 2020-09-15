d88cec50a1067bf7a009f75f8468ad6f9a788bea
kukovisuals <clubhousenet@live.com>
2020-09-14 20:53:50 -0400

    added amazon shopping cart excersice

--- functionalProgramming/exAmazon.js
+++ functionalProgramming/exAmazon.js
@@ -1,49 +1,62 @@
-	// amazon shopping
-	const user = {
-		name: 'Kim',
-		active: true,
-		cart: [],
-		purchases: []
-	}
-	const item = {
-			name: 'dildo',
-			price: 500
-		} 
+// amazon shopping
+const user = {
+	name: 'Kim',
+	active: true,
+	cart: [],
+	purchases: []
+}
+const item = {
+		name: 'dildo',
+		price: 500
+	} 
 
-	const compose = (f,g) => (...arg) => f(g(...arg));
+const amazonHistory = []
+const compose = (f,g) => (...arg) => f(g(...arg));
 
-	purchaseItem(
-		emptyCart,
-		buyItem,
-		applyTaxToItems,
-		addItemToCart
-	)(user, item)
+purchaseItem(
+	emptyCart,
+	buyItem,
+	applyTaxToItems,
+	addItemToCart
+)(user, item)
 
-	function purchaseItem(...fns) {
-		return fns.reduce(compose)
-	}
+function purchaseItem(...fns) {
+	return fns.reduce(compose)
+}
 
-	function addItemToCart(user, item){
-		const updatedCart = user.cart.concat([item])
-		return Object.assign({}, user, {cart: updatedCart})
-	}
+function addItemToCart(user, item){
+	amazonHistory.push(user)
+	const updatedCart = user.cart.concat([item])
+	return Object.assign({}, user, {cart: updatedCart})
+}
 
-	function applyTaxToItems(user){
-		const {cart} = user;
-		const taxtRate = 1.3;
-		const updatedCart = cart.map( item => {
-			return {
-				name: item.name,
-				price: item.price * taxtRate
-			}
-		})
-		return Object.assign({}, user, {cart: updatedCart})
-	}
+function applyTaxToItems(user){
+	amazonHistory.push(user)
+	const {cart} = user;
+	const taxtRate = 1.3;
+	const updatedCart = cart.map( item => {
+		return {
+			name: item.name,
+			price: item.price * taxtRate
+		}
+	})
+	return Object.assign({}, user, {cart: updatedCart})
+}
 
-	function buyItem(user){
-		return Object.assign({}, user, {purchases: user.cart})
-	}
+function buyItem(user){
+	amazonHistory.push(user)
+	return Object.assign({}, user, {purchases: user.cart})
+}
 
-	function emptyCart(user){
-		return Object.assign({}, user, {cart: []})
-	}
+function emptyCart(user){
+	amazonHistory.push(user)
+	return Object.assign({}, user, {cart: []})
+}
+
+amazonHistory
+
+
+
+
+
+
